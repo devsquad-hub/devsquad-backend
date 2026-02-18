73b6b805bb8fefe79f0f1d11699857daa7fd3b79
filipemartinsdev <dev.filipemartins@gmail.com>
2026-02-18 13:56:55 -0300

    chore: remove postgresql container

---
+++ src/test/java/com/spring/devsquad/DevsquadHubApplicationTest.java
@@ -0,0 +1,12 @@
+package com.spring.devsquad;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DevsquadHubApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
