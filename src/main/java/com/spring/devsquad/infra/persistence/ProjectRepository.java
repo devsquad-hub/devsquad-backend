b8b125c3926fddd10a5a4cf6673388303d303dc1
filipemartinsdev <dev.filipemartins@gmail.com>
2026-02-18 14:38:34 -0300

    feat(project): create JPQL queries to retrieve ProjectSummary

--- src/main/java/com/spring/devsquad/infra/persistence/ProjectRepository.java
+++ src/main/java/com/spring/devsquad/infra/persistence/ProjectRepository.java
@@ -5,6 +5,7 @@ import com.spring.devsquad.domain.project.ProjectSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
@@ -12,8 +13,42 @@ import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
 //    TODO create JPQL query
+        @Query(
+        """
+        SELECT p.id AS id,
+               p.title AS title,
+               p.description AS description,
+               p.techStack AS techStack,
+               p.status AS status,
+               p.userAuthor AS userAuthor,
+               COUNT(DISTINCT sa) AS candidatesCount
+               FROM Project p
+               
+               LEFT JOIN SquadApplication sa
+               ON sa.project.id = p.id
+               
+               WHERE p.id = :projectId
+               GROUP BY p.id
+        """
+)
     Optional<ProjectSummary> findProjectSummaryById(@Param("projectId") UUID id);
 
 //    TODO create JPQL query
+    @Query(
+        """
+        SELECT p.id AS id,
+               p.title AS title,
+               p.description AS description,
+               p.techStack AS techStack,
+               p.status AS status,
+               p.userAuthor AS userAuthor,
+               COUNT(DISTINCT sa) AS candidatesCount
+               FROM Project p
+               
+               LEFT JOIN SquadApplication sa
+               ON sa.project.id = p.id
+               GROUP BY p.id
+        """
+    )
     Page<ProjectSummary> findAllProjectSummary(Pageable pageable);
 }
